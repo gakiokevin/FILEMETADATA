1c1b245caa25130a1705565382b3522f56a36e79
gakiokevin <gakiokevin5@gmail.com>
2024-02-29 17:20:58 +0300

    a metadata app that takes in file and display it input

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
 # File Metadata Microservice
 
-This is the boilerplate for the File Metadata Microservice project. Instructions for building your project can be found at https://www.freecodecamp.org/learn/apis-and-microservices/apis-and-microservices-projects/file-metadata-microservice
+This is folder that contain a web application which you uplaod a file and an api return the metadata on the file that was uploaded
--- index.js
+++ index.js
@@ -1,20 +1,48 @@
-var express = require('express');
-var cors = require('cors');
+const  express =  require('express')
+const cors = require('cors')
+const multer =  require('multer')
+const bodyParser = require('body-parser')
 require('dotenv').config()
+const PORT  =  process.env.PORT ||3000
+const path = require('path');
 
-var app = express();
 
-app.use(cors());
-app.use('/public', express.static(process.cwd() + '/public'));
+const app = express()
 
-app.get('/', function (req, res) {
-  res.sendFile(process.cwd() + '/views/index.html');
+app.use(express.static(path.join(__dirname, 'views')));
+app.use(cors())
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: true }));
+
+const storage = multer.memoryStorage(); 
+const upload = multer({ storage: storage });
+
+
+
+app.post('/upload-file',upload.single('file'),uploadFile)
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'views', 'index.html'));
 });
 
 
+function uploadFile(req,res,){
+   const file =  req.file
+
+   if(!file){
+    return res.status(400).json({ error: 'No file uploaded.' });
+   }
+
+   const fileSize = file.size
+   const fileType = file.mimetype
+   const fileName = file.originalname
+      
+
+   res.json({name:fileName,type:fileType,size:fileSize})
 
 
-const port = process.env.PORT || 3000;
-app.listen(port, function () {
-  console.log('Your app is listening on port ' + port)
-});
+}
+
+
+
+app.listen(PORT,()=>console.log('server has started'))
